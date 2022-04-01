c78a21f432e9d09fc892ed6d0ba41e43d1b1d558
katerina2018 <43490511+katerina2018@users.noreply.github.com>
2022-04-01 10:50:45 +0300

    update

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -5,7 +5,7 @@ import FeedbackOptions from '../FeedbackOptions';
 import Notification from '../Notification';
 import Statistics from '../Statistics';
 
-import './Feedback.css';
+
 
 const Feedback = () => {
   const [good, setGood] = useState(0);
@@ -65,59 +65,4 @@ const Feedback = () => {
 
 export default Feedback;
 
-// class Feedback extends React.Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-//   onLeaveFeedback = label => {
-//     this.setState(prevState => ({
-//       [label]: prevState[label] + 1,
-//     }));
-//   };
 
-//   countTotalFeedback = () => {
-//     const totalFeedback = Object.values(this.state);
-//     return totalFeedback.reduce((previousValue, number) => {
-//       return previousValue + number;
-//     }, 0);
-//   };
-
-//   countPositiveFeedbackPercentage = () => {
-//     return this.state.bad
-//       ? Number.parseInt(
-//           100 - (this.state.bad * 100) / (this.state.bad + this.state.good),
-//         )
-//       : 100;
-//   };
-
-//   render() {
-//     return (
-//       <>
-//         <Section title="Please leave feedback">
-//           <FeedbackOptions
-//             options={this.state}
-//             onLeaveFeedback={this.onLeaveFeedback}
-//           />
-//         </Section>
-
-//         <Section title="Statistics">
-//           {!this.countTotalFeedback() ? (
-//             <Notification message="There is no feedback" />
-//           ) : (
-//             <Statistics
-//               good={this.state.good}
-//               neutral={this.state.neutral}
-//               bad={this.state.bad}
-//               total={this.countTotalFeedback()}
-//               positivePercentage={this.countPositiveFeedbackPercentage()}
-//             />
-//           )}
-//         </Section>
-//       </>
-//     );
-//   }
-// }
-
-// export default Feedback;
